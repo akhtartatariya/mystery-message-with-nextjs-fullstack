c3bdda18316936e8f14a5cde52e66f0abad34342
Akhtar <137376031+akhtartatariya@users.noreply.github.com>
2024-07-18 16:41:48 +0530

    all working properly

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      y e dashobad
+    </div>
+  )
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,15 @@ export { default } from "next-auth/middleware"
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
 
-    const token = await getToken({req:request})
+    const token = await getToken({ req: request })
     const url = request.nextUrl
-    if (token && (
-        url.pathname.startsWith('/sign-in') ||
-        url.pathname.startsWith('/sign-up') ||
-        url.pathname.startsWith('/verify') ||
-        url.pathname.startsWith('/')
-
-    )) {
+    if (
+        token &&
+        (url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify') ||
+            url.pathname === '/')
+    ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
     if (!token && url.pathname.startsWith('/dashboard')) {
@@ -24,12 +24,12 @@ export async function middleware(request: NextRequest) {
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-    matcher: [
-        '/',
-        '/sign-in',
-        '/sign-up',
-        '/dashboard/:path*',
-        '/verify/:path*'
-    ]
-}
+// export const config = {
+//     matcher: [
+//         '/',
+//         '/sign-in',
+//         '/sign-up',
+//         '/dashboard/:path*',
+//         '/verify/:path*'
+//     ]
+// }
